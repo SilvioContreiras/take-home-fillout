e7c87639e2f974204b21abaa22bdfb07736483d6
Sílvio Contreiras <silviocontreiras@MacBook-Pro-de-Silvio.local>
2025-06-29 16:28:17 -0300

    feat: add icons

---
+++ src/components/icons/icons-props.ts
@@ -0,0 +1,10 @@
+import React from "react";
+
+export interface IconProps {
+  color?: string;
+  width?: number;
+  height?: number;
+  size?: number; 
+  style?: React.CSSProperties;
+  className?: string; 
+}
---
+++ src/components/icons/pages/index.ts
@@ -0,0 +1,3 @@
+export * from './details'
+export * from './ending'
+export * from './info'
