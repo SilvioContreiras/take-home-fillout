e3c6971bf746c39abf2a3fe8a55ad9ab55147676
Sílvio Contreiras <silviocontreiras@MacBook-Pro-de-Silvio.local>
2025-06-29 12:38:29 -0300

    feat: configs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,20 @@
+import './globals.css';
+import { nunito, inter } from './fonts';
+
+export const metadata = {
+  title: 'Fillout Form Builder',
+  keywords: ['form builder', 'react forms', 'next.js forms'],
+  description: 'Fillout Form Builder',
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
-}>) {
+} >) {
   return (
-    <html lang="en">
-      <body className={'antialiased'}>
-        {children}
-      </body>
+    <html lang="en" className={`antialiased ${nunito.variable} ${inter.variable}`}>
+      <body className="font-sans">{children}</body>
     </html>
   );
 }
